ea39bc242ab874348bbb2d0626bc60172f2d101b
Vladislav Kuznetsov <Vladislav.Kuznetsov@jetbrains.com>
2022-03-07 01:10:29 +0300

    Add 3, 5 tasks

--- gradient.py
+++ gradient.py
@@ -1,7 +1,9 @@
 import numpy as np
-from numpy import ndarray
 from matplotlib import pyplot as plt
+from numpy import ndarray
 
+import steps
+from drawing import draw1d, draw2d
 from graphic import Graphic, RandomGraphic
 from onedimsearch import OneDimensionSearch
 from steps import StepScheduler, RateStep, LinearStep, ParabolicStep
@@ -28,6 +30,22 @@ def gradient_steps(
     return points
 
 
+def binary_rises(
+        function: Graphic,
+        one_dimension: OneDimensionSearch,
+        eps: float,
+        current_position: ndarray,
+        limit: int,
+        def_fun
+):
+    top = 10
+    while top < limit:
+        if def_fun(function, one_dimension, top, eps, current_position, False) is not None:
+            return def_fun(function, one_dimension, top * 10, eps, current_position, False)
+        top *= 2
+    return None
+
+
 def find_best_learning_rate_gradient_steps(
         function: Graphic,
         one_dimension: OneDimensionSearch,
@@ -47,17 +65,23 @@ def find_best_const_schedule(
         one_dimension: OneDimensionSearch,
         max_epoch: int,
         eps: float,
-        current_position: ndarray
+        current_position: ndarray,
+        drawing: bool
 ):
-    const_steps = np.linspace(0.01, 1, 100)
+    const_steps = np.linspace(0.01, 1, 70)
     epoches_const = np.array(
         [(gradient_steps(function, RateStep(lr), one_dimension, max_epoch, eps, current_position).size // 2) for lr in
          const_steps])
     best_const = epoches_const.argmin()
 
-    print(f"Const: Rate={const_steps[best_const]} with {epoches_const[best_const]} steps")
+    if drawing:
+        print(f"Const: Rate={const_steps[best_const]} with {epoches_const[best_const]} steps")
+        draw1d(const_steps, epoches_const, best_const)
 
-    draw1d(const_steps, epoches_const, best_const)
+    if epoches_const[best_const] != max_epoch:
+        return steps.RateStep(const_steps[best_const])
+    else:
+        return None
 
 
 def find_best_linear_schedule(
@@ -65,20 +89,26 @@ def find_best_linear_schedule(
         one_dimension: OneDimensionSearch,
         max_epoch: int,
         eps: float,
-        current_position: ndarray
+        current_position: ndarray,
+        drawing: bool
 ):
-    linear_starts = np.linspace(0.01, 0.8, 80)
-    linear_steps = np.linspace(0.97, 0.999, 30)
+    linear_starts = np.linspace(0.01, 0.8, 20)
+    linear_steps = np.linspace(0.97, 0.999, 10)
     epoches_linear = np.array(
         [[gradient_steps(function, LinearStep(ls, step), one_dimension, max_epoch, eps, current_position)
               .size // 2 for ls in linear_starts] for step in
          linear_steps])
     best_linear = np.unravel_index(epoches_linear.argmin(), epoches_linear.shape)
 
-    print(
-        f"Linear: Start={linear_starts[best_linear[1]]}, Step={linear_steps[best_linear[0]]} with {epoches_linear[best_linear]} steps")
+    if drawing:
+        print(
+            f"Linear: Start={linear_starts[best_linear[1]]}, Step={linear_steps[best_linear[0]]} with {epoches_linear[best_linear]} steps")
+        draw2d(linear_starts, linear_steps, epoches_linear, best_linear)
 
-    draw2d(linear_starts, linear_steps, epoches_linear, best_linear)
+    if epoches_linear[best_linear] != max_epoch:
+        return steps.LinearStep(linear_starts[best_linear[1]], linear_steps[best_linear[0]])
+    else:
+        return None
 
 
 def find_best_parabolic_schedule(
@@ -86,33 +116,24 @@ def find_best_parabolic_schedule(
         one_dimension: OneDimensionSearch,
         max_epoch: int,
         eps: float,
-        current_position: ndarray
+        current_position: ndarray,
+        drawing: bool
 ):
-    parabaloic_k = np.linspace(1, 300, 300)
+    parabaloic_k = np.linspace(1, 3000, 70)
     epoches_parabaloic = np.array(
         [(gradient_steps(function, ParabolicStep(k), one_dimension, max_epoch, eps, current_position).size // 2) for k
          in
          parabaloic_k])
     best_parabaloic = epoches_parabaloic.argmin()
 
-    print(f"Parabaloic: K={parabaloic_k[best_parabaloic]} with {epoches_parabaloic[best_parabaloic]} steps")
+    if drawing:
+        print(f"Parabaloic: K={parabaloic_k[best_parabaloic]} with {epoches_parabaloic[best_parabaloic]} steps")
+        draw1d(parabaloic_k, epoches_parabaloic, best_parabaloic)
 
-    draw1d(parabaloic_k, epoches_parabaloic, best_parabaloic)
-
-
-def draw1d(linspace, epoches, point=None):
-    plt.plot(linspace, epoches)
-    if point is not None:
-        plt.axvline(linspace[point], color="r")
-    plt.show()
-
-
-def draw2d(x_linspace, y_linspace, epoches, point=None):
-    X, Y = np.meshgrid(x_linspace, y_linspace)
-    if point is not None:
-        plt.plot(x_linspace[point[1]], y_linspace[point[0]], marker="o", markerfacecolor="red")
-    plt.contourf(X, Y, epoches)
-    plt.show()
+    if epoches_parabaloic[best_parabaloic] != max_epoch:
+        return steps.ParabolicStep(parabaloic_k[best_parabaloic])
+    else:
+        return None
 
 
 def test_task6(
--- kmain.py
+++ kmain.py
@@ -1,8 +1,74 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
+import gradient
+import graphic
 import onedimsearch
-from gradient import fund_best_schedule
-from graphic import F, F2
+import steps
+
+
+def _01():
+    gold_split_const_best = gradient.find_best_const_schedule(graphic.F(), onedimsearch.GoldSplit(), 1000, 1e-5,
+                                                              np.array([15, 15]),
+                                                              False)
+    gold_split_linear_best = gradient.find_best_linear_schedule(graphic.F(), onedimsearch.GoldSplit(), 1000, 1e-5,
+                                                                np.array([15, 15]), False)
+    gold_split_parabolic_best = gradient.find_best_parabolic_schedule(graphic.F(), onedimsearch.GoldSplit(), 1000, 1e-5,
+                                                                      np.array([15, 15]), False)
+
+    print(steps.RateStep(gold_split_const_best), steps.LinearStep(*gold_split_linear_best),
+          steps.ParabolicStep(gold_split_parabolic_best))
+
+
+def _02():
+    eps = 1e-4
+
+    for search in [onedimsearch.StepByStep(), onedimsearch.GoldSplit()]:
+        for g in [graphic.F(), graphic.G(), graphic.RandomGraphic(2)]:
+            print(search.__class__, g.__class__)
+            for type_best in [gradient.find_best_const_schedule, gradient.find_best_linear_schedule,
+                              gradient.find_best_parabolic_schedule]:
+                gradient.binary_rises(
+                    g,
+                    search,
+                    eps,
+                    np.array([15, 15]),
+                    100000,
+                    type_best
+                )
+
+def _03():
+    graphics = []
+
+    for _ in range(5):
+        graphics.append(graphic.RandomGraphic(2))
+
+    for g in graphics:
+        g.draw()
+        for search_method in [onedimsearch.StepByStep(), onedimsearch.GoldSplit()]:
+            print(search_method.__class__)
+            for type_best in [gradient.find_best_const_schedule, gradient.find_best_linear_schedule,
+                              gradient.find_best_parabolic_schedule]:
+                print(type_best.__name__)
+                best = gradient.binary_rises(
+                    g,
+                    search_method,
+                    1e-5,
+                    np.array([15, 15]),
+                    10000,
+                    type_best
+                )
+                points = gradient.gradient_steps(g, best, search_method, 10000, 1e-5, np.array([15, 15]))
+                x = points[:, 0]
+                y = points[:, 1]
+                x_lin = np.linspace(x.min(), x.max(), 100)
+                y_lin = np.linspace(y.min(), y.max(), 100)
+                X, Y = np.meshgrid(x_lin, y_lin)
+                plt.plot(points[:, 0], points[:, 1], "o-")
+                # plt.contour(X, Y, levels=sorted(list(set([g(np.array([p[0], p[1]])) for p in points] + list(
+                # np.linspace(-1, 1, 100))))))
+                plt.show()
+
 
 if __name__ == '__main__':
-    fund_best_schedule(F2(), onedimsearch.GoldSplit(), 1000, 1e-3, np.array([15, 15]))
+    _03()
